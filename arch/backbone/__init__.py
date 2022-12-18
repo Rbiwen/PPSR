0e3207637843392b9581fae088947bb28f5e3e1d
zh-hike <1583124882@qq.com>
2022-12-18 14:59:06 +0800

    初始化仓库

---
+++ arch/backbone/__init__.py
@@ -0,0 +1,8 @@
+import paddle.nn as nn
+
+
+class TheseusLayer(nn.Layer):
+    def __init__(self, **kwargs):
+        super(TheseusLayer, self).__init__(**kwargs)
+
+    
---
+++ engine/engine.py
@@ -0,0 +1,6 @@
+import paddle
+
+
+class Engine:
+    def __init__(self, config):
+        self.cfg = config
